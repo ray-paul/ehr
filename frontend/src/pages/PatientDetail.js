44dca45eac06f4122d7c96d748ba264c9a24fd95
ray-paul <h210632f@hit.ac.zw>
2025-11-19 01:06:35 +0200

    fix: remove duplicate code in ReportsCard (compile error)

--- frontend/src/pages/PatientDetail.js
+++ frontend/src/pages/PatientDetail.js
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import ReportsCard from '../components/ReportsCard';
 import api from '../services/api';
