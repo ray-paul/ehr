c9e63898b8579041b2d7d749515bb629d8c5c8ff
ray-paul <h210632f@hit.ac.zw>
2026-02-24 10:05:32 +0200

    fixed timedelta error

--- backend/ehr_project/settings.py
+++ backend/ehr_project/settings.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import os
 from decouple import config
 import dj_database_url
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
